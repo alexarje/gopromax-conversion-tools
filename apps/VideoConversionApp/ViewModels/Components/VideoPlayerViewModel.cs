3b7e36fe8510f5387ac3614f64a549e18bb54712
Jussi Saarivirta <jusasi@gmail.com>
2025-05-27 21:56:45 +0300

    Rotatable 3D view in VLC video view, and other fun stuff.

--- apps/VideoConversionApp/ViewModels/Components/VideoPlayerViewModel.cs
+++ apps/VideoConversionApp/ViewModels/Components/VideoPlayerViewModel.cs
@@ -1,43 +1,20 @@
 using System;
 using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using LibVLCSharp.Shared;
 
 namespace VideoConversionApp.ViewModels.Components;
 
-public class VideoPlayerViewModel : ViewModelBase
+public partial class VideoPlayerViewModel : ViewModelBase
 {
-    private readonly LibVLC _libVlc = new LibVLC();
-        
-    public MediaPlayer MediaPlayer { get; }
 
+    [ObservableProperty]
+    public partial Uri VideoUri { get; set; } 
+        
     public VideoPlayerViewModel()
     {
-        MediaPlayer = new MediaPlayer(_libVlc);
+        VideoUri = new Uri("https://streams.videolan.org/streams/360/eagle_360.mp4");
     }
     
-    public void Play()
-    {
-        if (Design.IsDesignMode)
-        {
-            return;
-        }
-            
-        using var media = new Media(_libVlc, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
-        //media.Duration
-        
-        MediaPlayer.Play(media);
-    }
-    
-    public void Stop()
-    {            
-        MediaPlayer.Stop();
-    }
-
-
-    public void Dispose()
-    {
-        MediaPlayer?.Dispose();
-        _libVlc?.Dispose();
-        
-    }
 }
--- apps/VideoConversionApp/Views/Components/VideoPlayerView.axaml.cs
+++ apps/VideoConversionApp/Views/Components/VideoPlayerView.axaml.cs
@@ -1,37 +1,138 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
+using LibVLCSharp.Avalonia;
+using LibVLCSharp.Shared;
 using VideoConversionApp.ViewModels;
 using VideoConversionApp.ViewModels.Components;
 
 namespace VideoConversionApp.Views.Components;
 
-public partial class VideoPlayerView : UserControl
+public partial class VideoPlayerView : UserControl, IDisposable
 {
+
+    private bool _isPanning = false;
+    private bool _isRolling = false;
+    private Point _previousMousePosition;
+    private float _fov = 80;
+    
+    private readonly LibVLC _libVlc = new LibVLC();
+    private MediaPlayer _mediaPlayer;
+
+    private VideoPlayerViewModel? ViewModel => DataContext as VideoPlayerViewModel;
+    
     public VideoPlayerView()
     {
         InitializeComponent();
+        _mediaPlayer = new MediaPlayer(_libVlc);
+        Player.MediaPlayer = _mediaPlayer;
         DataContext = new VideoPlayerViewModel(); // TODO temporary
-        ControlsPanel.IsVisible = true;
     }
     
     private void OnDataContextChanged(object sender, EventArgs e)
     {
-        
         if (DataContext is VideoPlayerViewModel vm)
         {
-            //vm.Play();
+            if(_mediaPlayer.Media != null)
+                _mediaPlayer.Media.Dispose();
+            
+            _mediaPlayer.Media = new Media(_libVlc, vm.VideoUri);
         }
     }
 
-    private void VideoViewOnPointerEntered(object sender, PointerEventArgs e)
+    private void VideoViewOnPointerPressed(object? sender, PointerPressedEventArgs e)
     {
-        //ControlsPanel.IsVisible = true;
+        var isRightClick = e.GetCurrentPoint(this).Properties.IsRightButtonPressed;
+        var isLeftClick = e.GetCurrentPoint(this).Properties.IsLeftButtonPressed;
+        
+        if (isLeftClick)
+        {
+            _isPanning = true;
+        }
+
+        if (isRightClick)
+        {
+            _isRolling = true;
+        }
+        
+
+        _previousMousePosition = e.GetCurrentPoint(this).Position;
     }
 
-    private void VideoViewOnPointerExited(object sender, PointerEventArgs e)
+    private void VideoViewOnPointerReleased(object? sender, PointerReleasedEventArgs e)
     {
-        //ControlsPanel.IsVisible = false;
+        _isPanning = false;
+        _isRolling = false;
     }
 
+    private void ControlsPanel_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (Player?.MediaPlayer == null)
+            return;
+        
+        var currentPosition = e.GetCurrentPoint(this).Position;
+        var movement = currentPosition - _previousMousePosition;
+        _previousMousePosition = currentPosition;
+
+        double range = Math.Max(Bounds.Width, Bounds.Height);
+        
+        // Left mouse button down drag: pitch and yaw.
+        if (_isPanning)
+        {
+            float yaw = (float)(_fov * -movement.X / range);
+            float pitch = (float)(_fov * -movement.Y / range);
+
+            var currentViewPoint = Player.MediaPlayer.Viewpoint;
+            Player.MediaPlayer.UpdateViewpoint(currentViewPoint.Yaw + yaw, currentViewPoint.Pitch + pitch, currentViewPoint.Roll, _fov);
+        }
+
+        // Right mouse button down: roll
+        if (_isRolling)
+        {
+            float roll = (float)(_fov * -movement.X / range);
+            var currentViewPoint = Player.MediaPlayer.Viewpoint;
+            Player.MediaPlayer.UpdateViewpoint(currentViewPoint.Yaw, currentViewPoint.Pitch, currentViewPoint.Roll + roll, _fov);
+        }
+        
+    }
+
+    private void StopClicked(object? sender, RoutedEventArgs e)
+    {
+        if (Player?.MediaPlayer == null)
+            return;
+        
+        Player.MediaPlayer.SeekTo(TimeSpan.Zero);
+        Player.MediaPlayer.Stop();
+        PlayPauseButton.IsChecked = false;
+    }
+
+    private void OnPlayPauseToggled(object? sender, RoutedEventArgs e)
+    {
+        if (Player?.MediaPlayer == null)
+            return;
+
+        var newCheckedState = PlayPauseButton.IsChecked;
+        
+        if (Player.MediaPlayer.IsPlaying && newCheckedState == false)
+            Player.MediaPlayer.Pause();
+        else if (!Player.MediaPlayer.IsPlaying && newCheckedState == true)
+            Player.MediaPlayer.Play();
+    }
+
+    public void Dispose()
+    {
+        // TODO this is not called automatically, see https://github.com/AvaloniaUI/Avalonia/discussions/6556
+        _libVlc.Dispose();
+        _mediaPlayer.Dispose();
+    }
+
+    private void ResetRotationClicked(object? sender, RoutedEventArgs e)
+    {
+        if (Player?.MediaPlayer == null)
+            return;
+
+        Player.MediaPlayer.UpdateViewpoint(0, 0, 0, _fov);
+    }
 }
