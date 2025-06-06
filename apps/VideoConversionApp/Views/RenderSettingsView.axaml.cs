0e872e21d490ee6a24128bb38567b8966be490fa
Jussi Saarivirta <jusasi@gmail.com>
2025-06-06 20:35:34 +0300

    Adding the Render tab content.

---
+++ apps/VideoConversionApp/Views/RenderSettingsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VideoConversionApp.Views;
+
+public partial class RenderSettingsView : UserControl
+{
+    public RenderSettingsView()
+    {
+        InitializeComponent();
+    }
+}
