9acb134c68ca742e23956b46d59166dd5c434c8d
Jussi Saarivirta <jusasi@gmail.com>
2025-06-07 19:39:19 +0300

    More visual things to the render queue.

--- apps/VideoConversionApp/ViewModels/RenderQueueViewModel.cs
+++ apps/VideoConversionApp/ViewModels/RenderQueueViewModel.cs
@@ -1,13 +1,25 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using VideoConversionApp.Utils;
 
 namespace VideoConversionApp.ViewModels;
 
-public class RenderQueueViewModel : ViewModelBase
+public partial class RenderQueueViewModel : ViewModelBase
 {
     public SortableObservableCollection<object> RenderQueue { get; } = new SortableObservableCollection<object>();
 
+    [ObservableProperty] public partial bool MinimalView { get; set; } = false;
+    
     public RenderQueueViewModel()
     {
+        RenderQueue.Add("item");
+        RenderQueue.Add("item");
+        RenderQueue.Add("item");
+        RenderQueue.Add("item");
+        RenderQueue.Add("item");
+        RenderQueue.Add("item");
+        RenderQueue.Add("item");
+        RenderQueue.Add("item");
+        RenderQueue.Add("item");
         RenderQueue.Add("item");
     }
     
